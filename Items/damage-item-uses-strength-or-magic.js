a62ca3cdfb2914561fd0679283d97d64b6e7c8e2
Marc Goldstein <markyjoe1990@gmail.com>
2020-04-25 19:23:54 -0400

    Update damage-item-uses-strength-or-magic.js

--- Items/damage-item-uses-strength-or-magic.js
+++ Items/damage-item-uses-strength-or-magic.js
@@ -1,5 +1,17 @@
 /*
-
+	Version 1.0
+	Made by MarkyJoe1990
+	
+	This plugin allows items with the "Damage" effect
+	to factor the user's strength or magic stat into the damage
+	formula.
+	
+	How to use:
+	- Create an item with the effect "Damage"
+	- Set the custom parameter "useStr" as true for physical attack
+	- Set the custom parameter "useMag" as true for magical attack
+	- Set both as true if you want to add both.
+	- Done
 */
 
 (function () {
