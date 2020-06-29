2be078bf48e8b020ae36029a5b836bad62d27962
Marc Goldstein <markyjoe1990@gmail.com>
2020-06-29 12:43:09 -0400

    Add files via upload

--- Skills/blows-riposte.js
+++ Skills/blows-riposte.js
@@ -95,18 +95,21 @@
 	var alias2 = DamageCalculator.calculateAttackPower;
 	DamageCalculator.calculateAttackPower = function(active, passive, weapon, isCritical, totalStatus, trueHitValue) {
 		var pow = alias2.call(this, active, passive, weapon, isCritical, totalStatus, trueHitValue);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(active) && skillArray[i].skill.custom.attack != undefined) {
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(active) && currentSkill.custom.attack != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
 				pow += skillArray[i].skill.custom.attack;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(active) && skillArray[i].skill.custom.attack != undefined) {
-				pow += skillArray[i].skill.custom.attack;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(active) && currentSkill.custom.attack != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				pow += currentSkill.custom.attack;
 			}
 		}
 		
@@ -116,18 +119,21 @@
 	var alias3 = DamageCalculator.calculateDefense;
 	DamageCalculator.calculateDefense = function (active, passive, weapon, isCritical, totalStatus, trueHitValue) {
 		var def = alias3.call(this, active, passive, weapon, isCritical, totalStatus, trueHitValue);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(passive) && skillArray[i].skill.custom.defense != undefined) {
-				def += skillArray[i].skill.custom.defense;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(passive) && currentSkill.custom.defense != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				def += currentSkill.custom.defense;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(passive) && skillArray[i].skill.custom.defense != undefined) {
-				def += skillArray[i].skill.custom.defense;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(passive) && currentSkill.custom.defense != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				def += currentSkill.custom.defense;
 			}
 		}
 		
@@ -137,18 +143,21 @@
 	var alias4 = HitCalculator.calculateSingleHit;
 	HitCalculator.calculateSingleHit = function(active, passive, weapon, totalStatus) {
 		var hit = alias4.call(this, active, passive, weapon, totalStatus);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(active) && skillArray[i].skill.custom.hit != undefined) {
-				hit += skillArray[i].skill.custom.hit;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(active) && currentSkill.custom.hit != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				hit += currentSkill.custom.hit;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(active) && skillArray[i].skill.custom.hit != undefined) {
-				hit += skillArray[i].skill.custom.hit;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(active) && currentSkill.custom.hit != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				hit += currentSkill.custom.hit;
 			}
 		}
 		
@@ -158,18 +167,21 @@
 	var alias5 = HitCalculator.calculateAvoid;
 	HitCalculator.calculateAvoid = function(active, passive, weapon, totalStatus) {
 		var avo = alias5.call(this, active, passive, weapon, totalStatus);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(passive) && skillArray[i].skill.custom.avoid != undefined) {
-				avo += skillArray[i].skill.custom.avoid;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(passive) && currentSkill.custom.avoid != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				avo += currentSkill.custom.avoid;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(passive) && skillArray[i].skill.custom.avoid != undefined) {
-				avo += skillArray[i].skill.custom.avoid;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(passive) && currentSkill.custom.avoid != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				avo += currentSkill.custom.avoid;
 			}
 		}
 		
@@ -179,18 +191,21 @@
 	var alias6 = CriticalCalculator.calculateSingleCritical;
 	CriticalCalculator.calculateSingleCritical = function (active, passive, weapon, totalStatus) {
 		var crit = alias6.call(this, active, passive, weapon, totalStatus);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(active) && skillArray[i].skill.custom.critical != undefined) {
-				crit += skillArray[i].skill.custom.critical;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(active) && currentSkill.custom.critical != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				crit += currentSkill.custom.critical;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(active) && skillArray[i].skill.custom.critical != undefined) {
-				crit += skillArray[i].skill.custom.critical;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(active) && currentSkill.custom.critical != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				crit += currentSkill.custom.critical;
 			}
 		}
 		
@@ -200,18 +215,21 @@
 	var alias7 = CriticalCalculator.calculateCriticalAvoid;
 	CriticalCalculator.calculateCriticalAvoid = function(active, passive, weapon, totalStatus) {
 		var cavo = alias7.call(this, active, passive, weapon, totalStatus)
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(passive) && skillArray[i].skill.custom.criticalAvoid != undefined) {
-				cavo += skillArray[i].skill.custom.criticalAvoid;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(passive) && currentSkill.custom.criticalAvoid != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				cavo += currentSkill.custom.criticalAvoid;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(passive, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(passive) && skillArray[i].skill.custom.criticalAvoid != undefined) {
-				cavo += skillArray[i].skill.custom.criticalAvoid;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(passive) && currentSkill.custom.criticalAvoid != undefined && currentSkill.getTargetAggregation().isCondition(active)) {
+				cavo += currentSkill.custom.criticalAvoid;
 			}
 		}
 		
@@ -221,18 +239,21 @@
 	var alias8 = Calculator.calculateAttackCount;
 	Calculator.calculateAttackCount = function(active, passive, weapon) {
 		var cnt = alias8.call(this, active, passive, weapon);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(active) && skillArray[i].skill.custom.attackCount != undefined) {
-				cnt += skillArray[i].skill.custom.attackCount;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(active) && currentSkill.custom.attackCount != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				cnt += currentSkill.custom.attackCount;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(active, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(active) && skillArray[i].skill.custom.attackCount != undefined) {
-				cnt += skillArray[i].skill.custom.attackCount;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(active) && currentSkill.custom.attackCount != undefined && currentSkill.getTargetAggregation().isCondition(passive)) {
+				cnt += currentSkill.custom.attackCount;
 			}
 		}
 		
@@ -242,18 +263,21 @@
 	var alias9 = AbilityCalculator.getAgility;
 	AbilityCalculator.getAgility = function (unit, weapon) {
 		var agi = alias9.call(this, unit, weapon);
+		var currentSkill;
 		
 		var skillArray = SkillControl.getDirectSkillArray(unit, SkillType.CUSTOM, "Blow-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isBlowBonus(unit) && skillArray[i].skill.custom.agility != undefined) {
-				agi += skillArray[i].skill.custom.agility;
+			currentSkill = skillArray[i].skill
+			if (isBlowBonus(unit) && currentSkill.custom.agility != undefined) {
+				agi += currentSkill.custom.agility;
 			}
 		}
 		
 		var skillArray = SkillControl.getDirectSkillArray(unit, SkillType.CUSTOM, "Riposte-Skill");
 		for (i = 0; i < skillArray.length; i++) {
-			if (isRiposteBonus(unit) && skillArray[i].skill.custom.agility != undefined) {
-				agi += skillArray[i].skill.custom.agility;
+			currentSkill = skillArray[i].skill
+			if (isRiposteBonus(unit) && currentSkill.custom.agility != undefined) {
+				agi += currentSkill.custom.agility;
 			}
 		}
 		
