5d8ac6a92f6d24cec5e756010019bd095902dffe
Marc Goldstein <markyjoe1990@gmail.com>
2024-09-20 20:41:28 -0400

    Add files via upload

--- Plugin/Optimizations/disable-enemy-supports.js
+++ Plugin/Optimizations/disable-enemy-supports.js
@@ -1,40 +1,26 @@
 /*
 	Made by MarkyJoe1990
-	Version 1.0
+	Version 2.0
 	
-	With this plugin, you get the option to disable certain kinds of
-	enemy support bonuses. Specifically, enemies will not get support
-	bonuses obtained from their allies via support skills. The only
-	exception is if those allies are sub leaders, or leaders.
+	With this plugin, all enemies that have Mob level importance
+	will be skipped over whenever an enemy unit's support bonuses
+	are being calculated. This saves a ton of processing power.
+
+	For enemies and classes that need be treated as an exception,
+	simply open up the unit or class's custom parameters and add
+	{
+		supportException: true
+	}
 	
-	This is important, because the greatest cause of lag in SRPG
-	Studio is from calculating enemy support bonuses, which is done
-	every time you check an enemy's stats, or hover your cursor
-	over an enemy you're about to attack. Since it's generally bad
-	design to give enemies support skills, and since this causes such
-	a substantial amount of lag when lots of enemies are on the map,
-	this feature is generally a nuisance more than it helps.
-	
-	In order to use this plugin:
-	- Go to the map where you want to disable enemy support bonuses
-	- Go to map information.
-	- Go to custom parameters
-	- Then put {disableEnemySupport: true}
-	
+	This plugin does not overwrite any functions.
 */
 
 (function () {
-	
-	//skips enemy skill support status collection EXCEPT for sub leaders and leaders
-	//if the map has enemy supports set to be disabled.
 	var alias2 = SupportCalculator._collectSkillStatus;
 	SupportCalculator._collectSkillStatus = function (unit, totalStatus) {
 		var unitType = unit.getUnitType();
-		var disableEnemySupport = root.getCurrentSession().getCurrentMapInfo().custom.disableEnemySupport;
 		
-		if (unitType == UnitType.ENEMY && disableEnemySupport) {
-			//Grab all sub leaders and leaders from the enemy list
-			
+		if (unitType == UnitType.ENEMY) {
 			var i, j, list, count, targetUnit, importance;
 			var listArray = this._getListArray(unit);
 			var listCount = listArray.length;
@@ -46,7 +32,9 @@
 					targetUnit = list.getData(j);
 					importance = targetUnit.getImportance();
 					if (unit === targetUnit || importance == ImportanceType.MOB) {
-						continue;
+						if (!targetUnit.custom.supportException && !targetUnit.getClass().custom.supportException) {
+							continue;
+						}
 					}
 					
 					this._checkSkillStatus(targetUnit, unit, false, totalStatus);
