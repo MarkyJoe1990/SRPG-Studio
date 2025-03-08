b7c4ceacc489075bc9a7f15054ce197897c517cd
Marc Goldstein <markyjoe1990@gmail.com>
2025-03-07 22:32:36 -0500

    Enemy AI Decision Optimizations

---
+++ Plugin/AI/AI-Decision-Optimizations/Distance.js
@@ -0,0 +1,33 @@
+/*
+    Enemy is encouraged to walk to tiles
+    that maximize the distance between them
+    and their target.
+*/
+
+( function () {
+    AIScorer.Distance = defineObject(BaseAIScorer, {
+        getScore: function(unit, combination) {
+            var targetUnit = combination.targetUnit;
+            if (targetUnit == null) {
+                return 0;
+            }
+
+            var index = combination.posIndex;
+            var unitX = CurrentMap.getX(index);
+            var unitY = CurrentMap.getY(index);
+            var targetX = targetUnit.getMapX();
+            var targetY = targetUnit.getMapY();
+
+            var distX = Math.abs(unitX - targetX);
+            var distY = Math.abs(unitY - targetY);
+
+            return (distX + distY) * 2;
+        }
+    });
+
+    var alias1 = CombinationSelector._configureScorerSecond;
+	CombinationSelector._configureScorerSecond = function(groupArray) {
+		alias1.call(this, groupArray);
+		groupArray.appendObject(AIScorer.Distance);
+	}
+}) ();
---
+++ Plugin/AI/AI-Decision-Optimizations/Movement.js
@@ -0,0 +1,18 @@
+/*
+    Enemy is encouraged to walk to tiles
+    that minimize movement spent.
+*/
+
+( function () {
+    AIScorer.Movement = defineObject(BaseAIScorer, {
+        getScore: function(unit, combination) {
+            return AIValue.MAX_MOVE - combination.movePoint;
+        }
+    });
+
+    var alias1 = CombinationSelector._configureScorerSecond;
+	CombinationSelector._configureScorerSecond = function(groupArray) {
+		alias1.call(this, groupArray);
+		groupArray.appendObject(AIScorer.Movement);
+	}
+}) ();
