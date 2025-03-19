74c09a83d08ca77cd5b537199949f65b8d4cc1af
Marc Goldstein <markyjoe1990@gmail.com>
2025-03-19 14:08:08 -0400

    Swarm AI Improvements By Repeat

--- Plugin/AI/Custom-AI/Swarm.js
+++ Plugin/AI/Custom-AI/Swarm.js
@@ -1,14 +1,15 @@
 /*
     Swarm AI
-    v1.0 by MarkyJoe1990
+    v1.2 by MarkyJoe1990, with modifications by Repeat
 
-    Enemy will attack targets if they are within
-    attack range of X of allies within group Y.
+    Enemy will approach & attack targets if they are
+    within the attack range of X of allies within
+    group Y.
 
     Simply set the AI to custom, with the Keyword
     "Swarm X Y" with X being the amount of enemies
     from group Y that the target needs to be in range
-    of to get the enemy to attack.
+    of to get the enemy to approach & attack.
 
     Y is NOT an integer, but a binary number. This is
     so you can have enemies as part of multiple groups
@@ -62,98 +63,114 @@
     AutoActionBuilder._buildSwarmAction = function(unit, autoActionArray, swarmCount, swarmGroup) {
         var combination = CombinationManager.getApproachCombination(unit, true);
         if (combination === null) {
-            return this._buildEmptyAction();
+            combination = CombinationManager.getEstimateCombination(unit);
+
+            if (combination === null) {
+                return this._buildEmptyAction();
+            } else {
+                // Enemy is not available to attack, but its teammates might be, 
+                // which means this unit wants to start approaching anyway.
+
+                if (isAISwarmAllowed(unit, combination, swarmCount, swarmGroup)) {
+                    // Set the target position to move.
+                    this._pushMove(unit, autoActionArray, combination);
+
+                    // Set it so as to wait after move.
+                    this._pushWait(unit, autoActionArray, combination);
+                } else {
+                    return this._buildEmptyAction();
+                }
+            }
         } else {
             // Enemy is available to attack, but how many
             // allies of group Y are they in range of?
-            var isSwarm = false;
-            var listArray = FilterControl.getListArray(FilterControl.getNormalFilter(unit.getUnitType()));
-            var i, keyword, keywordSplit, count = listArray.length;
-            var totalSwarmCount = 0;
-            var currentSwarmGroup;
-            // Filter out all allies not in the same swarm group
-            for (i = 0; i < count; i++) {
-                listArray[i] = AllUnitList.getList(listArray[i], function(currentUnit) {
-                    if (currentUnit === unit) {
-                        return true;
-                    }
-
-                    // Allow custom parameter option
-                    if ((currentUnit.custom.swarmGroup & swarmGroup) !== 0) {
-                        return true;
-                    }
-
-                    var aiPattern = currentUnit.createAIPattern();
-                    if (aiPattern == null) {
-                        return false;
-                    }
-
-                    if (aiPattern.getPatternType() !== PatternType.CUSTOM) {
-                        return false;
-                    }
-
-                    keyword = aiPattern.getCustomKeyword();
-                    keywordSplit = keyword.split(" ");
-                    if (keywordSplit[0] !== "Swarm") {
-                        return false;
-                    }
-
-                    currentSwarmCount = parseInt(keywordSplit[1]);
-                    if (isNaN(currentSwarmCount) === true) {
-                        root.msg("WARNING: " + currentUnit.getName() + " has a NaN swarm count.");
-                        return false;
-                    }
-
-                    currentSwarmGroup = parseInt(keywordSplit[2]);
-                    if (isNaN(currentSwarmGroup) === true) {
-                        root.msg("WARNING: " + currentUnit.getName() + " has a NaN swarm group.");
-                        return false;
-                    }
-
-                    return (currentSwarmGroup & swarmGroup) !== 0;
-                });
-
-                totalSwarmCount += listArray[i].getCount();
-            }
-
-
-            if (swarmCount > totalSwarmCount) {
-                swarmCount = totalSwarmCount;
-            }
-
-            // Now, grab the ranges of all remaining allies
-            // and see if they can attack the desired target.
-            var j, list, currentUnit, attackRange, indexArray, count2;
-            var simulator = root.getCurrentSession().createMapSimulator();
-            var availableSwarmCount = 0;
-            for (i = 0; i < count; i++) {
-                list = listArray[i];
-                count2 = list.getCount();
-                for (j = 0; j < count2; j++) {
-                    currentUnit = list.getData(j);
-                    attackRange = UnitRangePanel.getUnitAttackRange(currentUnit);
-                    simulator.startSimulationWeapon(currentUnit, attackRange.mov, attackRange.startRange, attackRange.endRange);
-                    indexArray = simulator.getSimulationWeaponIndexArray();
-                    if (IndexArray.findUnit(indexArray, combination.targetUnit) === true) {
-                        availableSwarmCount++;
-                        if (availableSwarmCount >= swarmCount) {
-                            isSwarm = true;
-                        }
-                    };
-                }
-
-                if (isSwarm === true) {
-                    break;
-                }
-            }
-
-            if (isSwarm === true) {
+            if (isAISwarmAllowed(unit, combination, swarmCount, swarmGroup)) {
                 this._pushGeneral(unit, autoActionArray, combination);
             } else {
                 return this._buildEmptyAction();
             }
-		}
+        }
 
         return true;
     }
-}) ();
+})();
+
+var isAISwarmAllowed = function(unit, combination, swarmCount, swarmGroup) {
+    var listArray = FilterControl.getListArray(FilterControl.getNormalFilter(unit.getUnitType()));
+    var i, keyword, keywordSplit, count = listArray.length;
+    var totalSwarmCount = 0;
+    var currentSwarmGroup;
+    // Filter out all allies not in the same swarm group
+    for (i = 0; i < count; i++) {
+        listArray[i] = AllUnitList.getList(listArray[i], function (currentUnit) {
+            if (currentUnit === unit) {
+                return true;
+            }
+
+            // Allow custom parameter option
+            if ((currentUnit.custom.swarmGroup & swarmGroup) !== 0) {
+                return true;
+            }
+
+            var aiPattern = currentUnit.createAIPattern();
+            if (aiPattern == null) {
+                return false;
+            }
+
+            if (aiPattern.getPatternType() !== PatternType.CUSTOM) {
+                return false;
+            }
+
+            keyword = aiPattern.getCustomKeyword();
+            keywordSplit = keyword.split(" ");
+            if (keywordSplit[0] !== "Swarm") {
+                return false;
+            }
+
+            currentSwarmCount = parseInt(keywordSplit[1]);
+            if (isNaN(currentSwarmCount) === true) {
+                root.msg("WARNING: " + currentUnit.getName() + " has a NaN swarm count.");
+                return false;
+            }
+
+            currentSwarmGroup = parseInt(keywordSplit[2]);
+            if (isNaN(currentSwarmGroup) === true) {
+                root.msg("WARNING: " + currentUnit.getName() + " has a NaN swarm group.");
+                return false;
+            }
+
+            return (currentSwarmGroup & swarmGroup) !== 0;
+        });
+
+        totalSwarmCount += listArray[i].getCount();
+    }
+
+
+    if (swarmCount > totalSwarmCount) {
+        swarmCount = totalSwarmCount;
+    }
+
+    // Now, grab the ranges of all remaining allies
+    // and see if they can attack the desired target.
+    var j, list, currentUnit, attackRange, indexArray, count2;
+    var simulator = root.getCurrentSession().createMapSimulator();
+    var availableSwarmCount = 0;
+    for (i = 0; i < count; i++) {
+        list = listArray[i];
+        count2 = list.getCount();
+        for (j = 0; j < count2; j++) {
+            currentUnit = list.getData(j);
+            attackRange = UnitRangePanel.getUnitAttackRange(currentUnit);
+            simulator.startSimulationWeapon(currentUnit, attackRange.mov, attackRange.startRange, attackRange.endRange);
+            indexArray = simulator.getSimulationWeaponIndexArray();
+            if (IndexArray.findUnit(indexArray, combination.targetUnit) === true) {
+                availableSwarmCount++;
+                if (availableSwarmCount >= swarmCount) {
+                    return true;
+                }
+            };
+        }
+    }
+
+    return false;
+};
