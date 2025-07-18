fc27f9f39779eb15b230f3423f833c67bc8fc0e6
Marc Goldstein <markyjoe1990@gmail.com>
2025-07-18 02:51:20 -0400

    Post-CPUIG Refactoring

--- Works In Progress/custom-parameter-ui-generator (attempt 2)/src/Components/Output.js
+++
@@ -1,9 +0,0 @@
-import OutputConverter from "./OutputConverter";
-
-function Output(props) {
-    return (
-        <textarea title="Output" className="Output" disabled value={ OutputConverter(props.outputArray, 0) }></textarea>
-    );
-}
-
-export default Output;
