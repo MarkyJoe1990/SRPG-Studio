dd62cd3302b98a64e6194146d742029e5e3c2341
Marc Goldstein <markyjoe1990@gmail.com>
2026-02-18 12:37:00 -0500

    Expanded Debug Menu

--- Plugin/Mechanics/Debug Menu/Debug-Menu-Objects.js
+++ Plugin/Mechanics/Debug Menu/Debug-Menu-Objects.js
@@ -31,7 +31,7 @@ var DebugMenu = defineObject(BaseListCommand, {
     },
 
     drawCommand: function() {
-        this._windowManager.drawWindowManager();
+        this.getCycleMode() === 0 && this._windowManager.drawWindowManager();
     },
 
     isCommandDisplayable: function() {
@@ -39,7 +39,7 @@ var DebugMenu = defineObject(BaseListCommand, {
             return true;
         }
 
-        return root.isTestPlay() && DebugMenuControl.getDebugEventArray().length > 0;
+        return root.isTestPlay();// && DebugMenuControl.getDebugEventArray().length > 0;
     },
 
     getObject: function() {
@@ -53,29 +53,60 @@ var DebugMenu = defineObject(BaseListCommand, {
 
 var DebugMenuWindowManager = defineObject(BaseWindowManager, {
     initialize: function() {
-        this._window = createWindowObject(DebugMenuWindow, this);
+        this._pageChanger = createObject(HorizontalPageChanger);
+        this._windowArray = [];
+        this._windowIndex = 0;
+        this._configureWindowArray(this._windowArray);
+
+        var currentWindow = this._windowArray[this._pageChanger.getPageIndex()];
+        this._pageChanger.setPageData(this._windowArray.length, currentWindow.getWindowWidth(), currentWindow.getWindowHeight());
     },
 
     moveWindowManager: function() {
-        return this._window.moveWindow();
+        this._pageChanger.movePage();
+        if (this._pageChanger.checkPage() === true) {
+            this._changePage(this._pageChanger.getPageIndex());
+        }
+
+        return this._windowArray[this._windowIndex].moveWindow();
+    },
+
+    _changePage: function(index) {
+        this._windowIndex = index;
+        var currentWindow = this._windowArray[this._windowIndex];
+
+        this._pageChanger.setPageData(this._windowArray.length, currentWindow.getWindowWidth(), currentWindow.getWindowHeight());
+        this._pageChanger._activePageIndex = this._windowIndex;
     },
 
     drawWindowManager: function() {
-        var x = LayoutControl.getCenterX(-1, this._window.getWindowWidth());
-        var y = LayoutControl.getCenterY(-1, this._window.getWindowHeight());
+        var x = LayoutControl.getCenterX(-1, this._windowArray[this._windowIndex].getWindowWidth());
+        var y = LayoutControl.getCenterY(-1, this._windowArray[this._windowIndex].getWindowHeight());
 
-        this._window.drawWindow(x, y);
+        this._pageChanger.drawPage(x, y);
+        this._windowArray[this._windowIndex].drawWindow(x, y);
     },
 
     getObject: function() {
-        return this._window.getObject();
+        return this._windowArray[this._windowIndex].getObject();
+    },
+
+    _configureWindowArray: function(groupArray) {
+        groupArray.appendWindowObject(DebugMenuDebugEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuAutoEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuOpeningEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuEndingEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuPlaceEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuTalkEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuCommunicationEventWindow, this);
+        groupArray.appendWindowObject(DebugMenuMapCommonEventWindow, this);
     }
 });
 
-var DebugMenuWindow = defineObject(BaseWindow, {
+var BaseDebugMenuWindow = defineObject(BaseWindow, {
     initialize: function() {
         this._scrollbar = createScrollbarObject(DebugMenuScrollbar, this);
-        var arr = DebugMenuControl.getDebugEventArray();
+        var arr = this._getEventArray();
         var count = arr.length;
         if (count > 10) {
             count = 10;
@@ -84,6 +115,14 @@ var DebugMenuWindow = defineObject(BaseWindow, {
         this._scrollbar.setScrollFormation(1, count);
         this._scrollbar.setObjectArray(arr);
 		this._scrollbar.setActive(true);
+    },
+
+    _getEventArray: function() {
+        return [];
+    },
+
+    _getEventList: function() {
+        return null;
     },
 
     moveWindowContent: function() {
@@ -104,7 +143,111 @@ var DebugMenuWindow = defineObject(BaseWindow, {
 
     getWindowHeight: function() {
         return this._scrollbar.getScrollbarHeight() + (this.getWindowYPadding() * 2);
+    },
+
+	getWindowTitleTextUI: function() {
+		return root.queryTextUI('objective_title');
+	},
+
+	_getWindowTitlePartsCount: function() {
+		return 6;
+	}
+});
+
+var BaseDebugMenuEventWindow = defineObject(BaseDebugMenuWindow, {
+    _getEventArray: function() {
+        var arr = [];
+
+        var list = this._getEventList();
+        var i, count = list.getCount();
+        for (i = 0; i < count; i++) {
+            arr.push(list.getData(i));
+        }
+
+        return arr;
     }
+});
+
+
+// Actual Objects
+var DebugMenuDebugEventWindow = defineObject(BaseDebugMenuWindow, {
+    _getEventArray: function() {
+        return DebugMenuControl.getDebugEventArray();
+    },
+
+	getWindowTitleText: function() {
+		return 'Debug Events';
+	}
+});
+
+var DebugMenuAutoEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getAutoEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Auto Events';
+	}
+});
+
+var DebugMenuOpeningEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getOpeningEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Opening Events';
+	}
+});
+
+var DebugMenuEndingEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getEndingEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Ending Events';
+	}
+});
+
+var DebugMenuPlaceEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getPlaceEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Place Events';
+	}
+});
+
+var DebugMenuTalkEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getTalkEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Talk Events';
+	}
+});
+
+var DebugMenuCommunicationEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getCommunicationEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Communication Events';
+	}
+});
+
+var DebugMenuMapCommonEventWindow = defineObject(BaseDebugMenuEventWindow, {
+    _getEventList: function() {
+        return root.getCurrentSession().getMapCommonEventList();
+    },
+
+	getWindowTitleText: function() {
+		return 'Map Common Events';
+	}
 });
 
 var DebugMenuScrollbar = defineObject(BaseScrollbar, {
